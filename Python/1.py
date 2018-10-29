6028966b8e86a2808a589d10ec8ada7dd963e921
韩彦曦 <hanyanxi@ziggurat.cn>
2018-10-29 14:39:18 +0800

    new multipage react app

---
+++ Python/1.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# item = input()
+# print('hello,', item + '\n', '''I\'m     
+# #  \"OK\" ''' )
+
+# n = '''r\'\'\'Hello,
+# Lisa!\'\'\''''
+# print(n)
+
+# s1 = 72
+# s2 = 85
+# s3 = (s2-s1)/s1 * 100
+# print('百分比是：%.1f %%' %s3 )
+
+# age = int(input('age:'))
+# if age > 20:
+#     print('your age is', age)
+# elif age < 25:
+#     print('hahaha')
+# else: 
+#     print('lolo')
+
+# sum = 0
+# for x in range(100):
+#     sum = sum + x
+# print(sum)
+
+# list = ['Bart', 'Lisa', 'Adam']
+# for name in list:
+#     print('Hello', name)
+
